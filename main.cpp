2a6aa985419586a0daf26b67f885910f65a8d883
Bernardo Zamperetti <bernardo.de-freitas-zamperetti@ensimag.grenoble-inp.fr>
2015-04-24 17:06:38 +0200

    fire torch pilar

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include "viewer.h"
 #include "dynamicSystem.h"
 #include "dragon.h"
+#include "fire.h"
 #include "conicalFrustrum.h"
+#include "pilar.h"
+#include "torch.h"
 
 
 int main(int argc, char** argv)
@@ -13,10 +16,19 @@ int main(int argc, char** argv)
 
 	// Instantiate the viewer.
 	Viewer viewer;
+//	Fire *fire = new Fire();
+//	fire->setIntensity(10.0f);
+
+
+	
 
 	// build your scene here
-    viewer.addRenderable(new Dragon());
+//   	viewer.addRenderable(new Dragon());
+//	viewer.addRenderable(new Fire(10));
 
+	
+    viewer.addRenderable(new Torch(2));
+	
 	viewer.setWindowTitle("viewer");
 	// Make the viewer window visible on screen.
 	viewer.show();
